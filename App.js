1ccd1f6d17d97f6f8209189cfc72499be4929267
DiepKinh <73871050+DiepKinh@users.noreply.github.com>
2021-10-11 17:12:08 +0700

    git init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}>ĐỖ THỊ THU DIỆP</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
