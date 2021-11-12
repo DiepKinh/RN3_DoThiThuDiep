095c8487385df170cde8abc6342c76879bc65407
DiepKinh <73871050+DiepKinh@users.noreply.github.com>
2021-11-12 16:09:29 +0700

    BTVN_Buoi12

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import DemoComponent from './src/Buoi01/DemoComponent';
 import Bidding from './src/Buoi02/Bidding';
 import LayoutZalo from './src/BaiTapPhu_LayoutZalo';
+import BTVN_Buoi12 from './src/BTVN_Buoi12';
 
 class App extends Component {
   render() {
-    return <LayoutZalo />;
+    return <BTVN_Buoi12 />;
   }
 }
 
